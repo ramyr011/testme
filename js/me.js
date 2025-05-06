2baad62ce57fef24ce6d0bf8f595508a4f799ad5
RAMy_Gendi <ramyadel2210@gmail.com>
2025-05-06 01:23:00 +0000

    Update me.js

--- js/me.js
+++ js/me.js
@@ -3,3 +3,17 @@ const toggleDarkBtn = document.getElementById('toggle-dark');
 toggleDarkBtn.addEventListener('change', () => {
               document.body.classList.toggle('dark-mode');
 });
+function searchProducts() {
+              const input = document.getElementById('searchInput');
+              const filter = input.value.toLowerCase();
+              const products = document.querySelectorAll('.product');
+
+              products.forEach(product => {
+                            const productName = product.querySelector('p').textContent.toLowerCase();
+                            if (productName.includes(filter)) {
+                                          product.style.display = '';
+                            } else {
+                                          product.style.display = 'none';
+                            }
+              });
+}
